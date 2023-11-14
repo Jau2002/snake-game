b219dbf2e73a61c6f49cd10b7e89af8ef680d82b
PC <PC@DESKTOP-OAJK77B>
2023-11-14 09:20:57 -0500

    Main Panel

--- src/Views/GamePanel.java
+++ src/Views/GamePanel.java
@@ -1,5 +1,17 @@
 package Views;
 
-public class GamePanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
